23acd6cd5347eb6207198497b9c173d185a984fd
Mariana <57249894+Ryana7-hub@users.noreply.github.com>
2020-05-01 18:14:54 +0300

    Add files via upload

---
+++ Cats-Univers-master/j.js
@@ -0,0 +1,20 @@
+function openArticle(evt, article) {
+    //declare all variables
+    var i, tabcontent, tablinks;
+
+    //get all elements with class = "tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    //get all elements with class = "tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    //show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(article).style.display = "block";
+    evt.currentTarget.className += " active";
+}
